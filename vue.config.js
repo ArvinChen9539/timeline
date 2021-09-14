74deb39a6be6a10eab12dd3f367059bfa1448c0e
陈敬 <chenjing@physicalpoint.com>
2021-09-14 19:22:47 +0800

    测试

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,29 @@
 const webpack = require('webpack');
 const pkg = require('./package.json');
-
+const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
 module.exports = {
+    publicPath: './',
     css: {
         //css打包在js文件中
         extract: false
     },
+    chainWebpack: config => {
+        const svgRule = config.module.rule('svg')
+        svgRule.uses.clear()
+        svgRule.use('svg-sprite-loader')
+            .loader('svg-sprite-loader')
+            .options({
+                extract:true
+            })
+        config.module
+            .rule('images')
+            .use('url-loader')
+            .loader('url-loader')
+            .tap(options => Object.assign(options, {limit: 20000}))
+    },
     configureWebpack: {
         plugins: [
+            new SpriteLoaderPlugin(),
             new webpack.BannerPlugin({
                 banner: pkg.name + ' - ' + pkg.description + '\n' +
                     '@version ' + pkg.version + '\n' +
