0e0fe2e73e5c1b16f93ea0859ea09a17407df57d
陈敬 <chenjing@physicalpoint.com>
2021-09-14 20:37:06 +0800

    修复horizontalStartPosition=bottom对齐的bug

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,11 @@
 ## 版本更新说明
 
+#### Version 1.0.4
+
+> 修复horizontalStartPosition=bottom时节点对齐有问题的BUG
+
 #### Version 1.0.0
+
 > 初始版本    
 > 改造为ES6导入的模式
 
--- src/demo/App.vue
+++ src/demo/App.vue
@@ -1,46 +1,53 @@
 <template>
-    <div>
+    <div class="timeline-content">
         <div class="timeline">
             <div class="timeline__wrap">
                 <div class="timeline__items">
                     <div class="timeline__item">
                         <div class="timeline__content">
-                            您的内容/标记
+                            公司成立
                         </div>
                     </div>
                     <div class="timeline__item">
                         <div class="timeline__content">
-                            您的内容/标记
+                            签约杭州市经信委
                         </div>
                     </div>
                     <div class="timeline__item">
                         <div class="timeline__content">
-                            您的内容/标记
+                            签约杭州市公安局<br/>
+                            签约泰一指尚数据产品<br/>
+                            高新<br/>
+                            双软<br/>
+                            “雏鹰计划”<br/>
                         </div>
                     </div>
-                      <div class="timeline__item">
+                    <div class="timeline__item">
                         <div class="timeline__content">
-                            您的内容/标记
+                            签约省高速交警数据中心<br/>
+                            签约华为数据营销<br/>
+                            签约网易数据中心<br/>
+                            CMMI3<br/>
+                            网易战略合作<br/>
                         </div>
                     </div>
-                      <div class="timeline__item">
+                    <div class="timeline__item">
                         <div class="timeline__content">
-                            您的内容/标记
+                            签约部分市级交通质监<br/>
+                            签约省交通质监数据中心<br/>
+                            “智慧质监”试点<br/>
                         </div>
                     </div>
-                      <div class="timeline__item">
+                    <div class="timeline__item">
                         <div class="timeline__content">
-                            您的内容/标记
+                            推广行业大数据应用解决方案<br/>
+                            华为战略合作<br/>
+                            腾讯合作伙伴<br/>
                         </div>
                     </div>
-                      <div class="timeline__item">
+                    <div class="timeline__item">
                         <div class="timeline__content">
-                            您的内容/标记
-                        </div>
-                    </div>
-                      <div class="timeline__item">
-                        <div class="timeline__content">
-                            您的内容/标记
+                            疫情大数据
                         </div>
                     </div>
                 </div>
@@ -56,13 +63,22 @@ import {Vue, Component} from 'vue-property-decorator'
 @Component
 export default class App extends Vue {
     mounted() {
-        timeline(document.querySelectorAll('.timeline'),{
+        timeline(document.querySelectorAll('.timeline'), {
             mode: 'horizontal',
+            horizontalStartPosition: 'bottom',
+            visibleItems: 7
         })
     }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.timeline-content {
+    width: 1200px;
+    height: 807px;
+    background-size: cover;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
 </style>
